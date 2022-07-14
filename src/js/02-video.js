fea231fe08de19ed0d86ff19d8bc89d8e2728589
BogdanGrynjuk <bgrynjuk@ukr.net>
2022-07-15 01:01:42 +0300

    Updates task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle'
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(function ({ seconds }) {
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}, 1000));
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+
+
+
+
+
+
+
+
 
