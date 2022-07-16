1c417c9e4f2c5a28b00854ba158a3ad1aced37c5
BogdanGrynjuk <bgrynjuk@ukr.net>
2022-07-17 01:38:20 +0300

    updates task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('.feedback-form input');
+const textareaMessage = document.querySelector('.feedback-form textarea');
+const dataForm = {};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+populateForm();
+
+function onFormInput(event) {  
+  dataForm[event.target.name] = event.target.value;  
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataForm));  
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();  
+  event.target.reset();
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function populateForm() {
+  const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (savedData.message) {
+      textareaMessage.value = savedData.message;
+  }
+
+  if (savedData.email) {
+      inputEmail.value = savedData.email;
+  }
+  
+  console.log(savedData)
+}
